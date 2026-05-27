eb3093a1d920f7a801f31835e589b453869ee06d
Guilherme <guimars22@gmail.com>
2026-05-27 15:53:34 -0300

    lists

---
+++ Python/Lista.py
@@ -0,0 +1,17 @@
+nome = "Guilherme"
+idade = 21
+
+minhaLista = ["Azul", "Vermelho", "Amarelo"]
+
+minhaLista.append("Roxo")
+minhaLista.append("Verde")
+
+minhaLista.remove("Vermelho")
+
+for cor in minhaLista:
+    print("Cor: ", cor)
+
+print(minhaLista[0:3])
+
+for letra in nome:
+    print(letra)
