1a6cc94684e5c6c54a8c2b71fca58dc1372bc482
Guilherme <guimars22@gmail.com>
2026-05-27 18:56:12 -0300

    add fuction `incluirContatos`

--- Python/Estrutura de dados/Agenda.py
+++ Python/Estrutura de dados/Agenda.py
@@ -13,14 +13,35 @@ AGENDA['giulia'] = {
     "endereco": "Rua 33",
 }
 
+
+
 def mostrarContatos():
     for contato in AGENDA:
-        print("Nome:     ", contato)
-        print("Telefone: ", AGENDA[contato]["telefone"])
-        print("Email:    ", AGENDA[contato]["email"])
-        print("Endereço: ", AGENDA[contato]["endereco"])
+        buscarContatos(contato)
         print('-------------------------------------')
         print()
 
 
-mostrarContatos()
+def buscarContatos(contato):
+    print("Nome:     ", contato)
+    print("Telefone: ", AGENDA[contato]["telefone"])
+    print("Email:    ", AGENDA[contato]["email"])
+    print("Endereço: ", AGENDA[contato]["endereco"])
+
+
+def incluirContatos(contato, telefone, email, endereco):
+    AGENDA[contato] = {
+        "telefone": telefone,
+        "email": email,
+        "endereco": endereco,
+    }
+    print(f">>>>>>>>>>>> Contato {contato} adicioando com sucesso =)")
+    print()
+
+
+## mostrarContatos()
+## incluirContatos("Marcella", "92313-2990", 'marcella@gmail.com', 'Rua 3')
+## mostrarContatos()
+
+## buscarContatos('guilherme')
+
