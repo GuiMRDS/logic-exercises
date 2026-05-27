f9562e30055fdc660647e79a2171f7d30031fbb7
Guilherme <guimars22@gmail.com>
2026-05-27 18:43:32 -0300

    add fuction `mostrarContatos`

--- Python/Estrutura de dados/Agenda.py
+++ Python/Estrutura de dados/Agenda.py
@@ -1,11 +1,26 @@
 print(" ------------------ Agenda ------------------ ")
+print()
 
 AGENDA = {}
+AGENDA['guilherme'] = {
+    "telefone": "99999-9999",
+    "email": "guimars22@gmail.com",
+    "endereco": "Rua 28",
+}
+AGENDA['giulia'] = {
+    "telefone": "99999-8888",
+    "email": "giulia@gmail.com",
+    "endereco": "Rua 33",
+}
+
+def mostrarContatos():
+    for contato in AGENDA:
+        print("Nome:     ", contato)
+        print("Telefone: ", AGENDA[contato]["telefone"])
+        print("Email:    ", AGENDA[contato]["email"])
+        print("Endereço: ", AGENDA[contato]["endereco"])
+        print('-------------------------------------')
+        print()
 
 
-def primeiro_metodo():
-    #TODO fazer primerio metodo
-    pass
-
-
-primeiro_metodo()
+mostrarContatos()
