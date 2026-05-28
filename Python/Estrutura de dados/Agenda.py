c691d2abf9eb7365cec1d00c0b2dffff9b40fe08
Guilherme <guimars22@gmail.com>
2026-05-28 13:59:05 -0300

    add menu and condicions

--- Python/Estrutura de dados/Agenda.py
+++ Python/Estrutura de dados/Agenda.py
@@ -1,5 +1,4 @@
 print(" ------------------ Agenda ------------------ ")
-print()
 
 AGENDA = {}
 AGENDA['guilherme'] = {
@@ -12,8 +11,6 @@ AGENDA['giulia'] = {
     "email": "giulia@gmail.com",
     "endereco": "Rua 33",
 }
-
-
 
 def mostrarContatos():
     for contato in AGENDA:
@@ -38,16 +35,50 @@ def incluirEditarContatos(contato, telefone, email, endereco):
     print(f">>>>>>>>>>>> Contato {contato} adicioando|editado com sucesso =)")
     print()
 
+
 def excluirContato(contato):
     AGENDA.pop(contato)
     print(f">>>>>>>>>>>> Contato {contato} excluido com sucesso =(")
 
 
-mostrarContatos()
-incluirEditarContatos("marcella", "92313-2990", 'marcella@gmail.com', 'Rua 3')
-incluirEditarContatos("guilherme", "95555-5550", 'guimars22@gmail.com', 'Rua 7')
-mostrarContatos()
-excluirContato('marcella')
-mostrarContatos()
+def  imprimir_menu():
+    print(' 1 - Mostrar contatos na Agenda ')
+    print(' 2 - Buscar contatos na Agenda ')
+    print(' 3 - Incluir contatos na Agenda ')
+    print(' 4 - Editar contatos na Agenda ')
+    print(' 5 - Excluir contatos na Agenda ')
+    print(' 0 - Sair da Agenda ')
 
-## buscarContatos('guilherme')
+
+imprimir_menu()
+opcao = input('Escolha uma opção: ')
+
+if opcao == '1':
+    print()
+    mostrarContatos()
+elif opcao == '2':
+    contato = input("Digite o nome do contato: ")
+    print()
+    buscarContatos(contato)
+elif opcao == '3':
+    print()
+    contato = input("Digite o nome do contato: ")
+    telefone = input("Digite o telefone do contato: ")
+    email = input("Digite o email do contato: ")
+    endereco = input("Digite o endereço do contato: ")
+    incluirEditarContatos(contato, telefone, email, endereco)
+elif opcao == '4':
+    print()
+    contato = input("Digite o nome do contato: ")
+    telefone = input("Digite o telefone do contato: ")
+    email = input("Digite o email do contato: ")
+    endereco = input("Digite o endereço do contato: ")
+    incluirEditarContatos(contato, telefone, email, endereco)
+elif opcao == '5':
+    print()
+    contato = input("Digite o nome do contato: ")
+    excluirContato(contato)
+elif opcao == '0':
+    print('Saindo do programa')
+else:
+    print("Opção invalida!")
