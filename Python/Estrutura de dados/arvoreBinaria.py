19c2f92f5278ba51742fa1bc61ff360e6e1f5109
Guilherme <guimars22@gmail.com>
2026-05-29 11:58:35 -0300

    #

--- Python/Estrutura de dados/arvoreBinaria.py
+++ Python/Estrutura de dados/arvoreBinaria.py
@@ -1,4 +1,23 @@
-class Node:
-    def  __init__(self, val=0, next = None):
+class TreeNode:
+    def  __init__(self, val=0, left=None, right=None):
         self.val = val
-        self.next = next
+        self.left = left
+        self.right = right
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(4)
+root.left.right.left = TreeNode(5)
+
+
+def postorder(node):
+    if not node:
+        return
+    postorder(node.left) # Abir esquerda
+    print(node.val, end=" ") # Abrir esquerda
+    postorder(node.right) # Abrir direita
+
+
+postorder(root)
