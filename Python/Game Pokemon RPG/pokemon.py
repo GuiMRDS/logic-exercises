570ba28559be2f0fd2793c6a69c82fc49a8c9148
Guilherme <guimars22@gmail.com>
2026-06-01 15:47:06 -0300

    #

--- Python/Game Pokemon RPG/pokemon.py
+++ Python/Game Pokemon RPG/pokemon.py
@@ -1,5 +1,4 @@
 class Pokemon:
-
     def __init__(self, tipo, especie):
         self.tipo = tipo
         self.especie = especie
@@ -7,9 +6,15 @@ class Pokemon:
     def __str__(self):
         return "{} ({})".format(self.especie, self.tipo)
 
+    def atacar(self, pokemon):
+        print("{} atacou {}!".format(self.especie, pokemon.especie))
+
 meu_pokemon = Pokemon("fogo", "charmander")
 meu_pokemon_do_visinho = Pokemon("eletrico", "pikachu")
 
 print(meu_pokemon)
-
 print(meu_pokemon_do_visinho)
+print()
+
+meu_pokemon_do_visinho.atacar(meu_pokemon)
+meu_pokemon.atacar(meu_pokemon_do_visinho)
