16316967258ec21c2f6e53e1502e488de44302a4
Guilherme <guimars22@gmail.com>
2026-06-05 11:59:22 -0300

    #

---
+++ Python/DSA/exponetial_search.py
@@ -0,0 +1,33 @@
+def binary_seacrh(nums, n, lo, hi):
+    while lo < hi:
+        mid = int((lo + hi) / 2)
+
+        if nums[mid] == n:
+            return mid
+        elif nums[mid] > n:
+            lo = mid + 1
+        else:
+            hi = mid
+    return -1
+
+
+def exponetial_seacrh(arr, target):
+    if arr[0] == target:
+        return 0
+    n = len(arr)
+    i = 1
+
+    while i <= n and arr[i] < target:
+        i *= 2
+
+    if arr[i] == target:
+        return i
+
+    return binary_seacrh(arr, target, i//2, min(i, n-1))
+
+
+arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+target = 32
+result = binary_seacrh(arr, target)
+
+print(f"Element found at index: {result}")
