a196b673d13467ee23f4324616781237cb118370
Guilherme <guimars22@gmail.com>
2026-06-05 11:39:12 -0300

    #

---
+++ Python/DSA/binary_seacrh.py
@@ -0,0 +1,21 @@
+def binary_seacrh(nums, n):
+    lo = 0
+    hi = len(nums) - 1
+    steps = 0
+
+    while lo < hi:
+        steps += 1
+        mid = int((lo + hi) / 2)
+
+        if nums[mid] == n:
+            print("step: ", steps)
+            return mid
+        elif nums[mid] > n:
+            lo = mid + 1
+        else:
+            hi = mid
+    return -1
+
+a = [1,2,3,4,5]
+
+binary_seacrh(a, 3)
