ead26ee8efd404517292235b6c982023090fc410
Guilherme <guimars22@gmail.com>
2026-06-09 12:11:07 -0300

    #

--- Python/DSA/linked.py
+++ Python/DSA/linked.py
@@ -4,70 +4,50 @@ class Node:
         self.next = None
         self.prev = None
 
-
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
 
-
     def add_to_front(self, value):
         new_node = Node(value)
-
-        new_node.next = self.head
-
-        if self.head:
-            self.head.prev = new_node
+        if not self.head:
+            self.head = self.tail = new_node
         else:
-            self.tail = new_node
-
-        self.head = new_node
-
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
 
     def add_to_end(self, value):
         new_node = Node(value)
-
-        new_node.prev = self.tail
-
-        if self.tail:
-            self.tail.next = new_node
+        if not self.tail:
+            self.head = self.tail = new_node
         else:
-            self.head = new_node
-
-        self.tail = new_node
-
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
     def remove_from_front(self):
         if not self.head:
             return None
-
         removed_value = self.head.value
-
-        self.head = self.head.next
-
-        if self.head:
-            self.head.prev = None
+        if self.head == self.tail:
+            self.head = self.tail = None
         else:
-            self.tail = None
-
+            self.head = self.head.next
+            self.head.prev = None
         return removed_value
-
 
     def remove_from_end(self):
         if not self.tail:
             return None
-
         removed_value = self.tail.value
-
-        self.tail = self.tail.prev
-
-        if self.tail:
-            self.tail.next = None
+        if self.head == self.tail:
+            self.head = self.tail = None
         else:
-            self.head = None
-
+            self.tail = self.tail.prev
+            self.tail.next = None
         return removed_value
-
 
 
 dll = DoublyLinkedList()
