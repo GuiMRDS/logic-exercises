364c086ad877ff74c3b721e5b37f6cc0879785c7
Guilherme <guimars22@gmail.com>
2026-07-29 11:45:15 -0300

    #

---
+++ Python/DSA/Binary Tree/Ex/94. Binary Tree Inoder Traversal.py
@@ -0,0 +1,47 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val):
+        if not self.root:
+            self.root = TreeNode(val)
+        else:
+            self._insert_recursive(self.root, val)
+
+    def _insert_recursive(self, node, val):
+        if val < node.val:
+            if node.left:
+                self._insert_recursive(node.left, val)
+            else:
+                node.left = TreeNode(val)
+        else:
+            if node.right:
+                self._insert_recursive(node.right, val)
+            else:
+                node.right = TreeNode(val)
+
+    def inoder_traversal(self):
+        result = []
+        self._inoder_traversal(self.root, result)
+        return result
+
+    def _inoder_traversal(self, node, result):
+        if node:
+            self._inoder_traversal(node.left, result)
+            result.append(node.val)
+            self._inoder_traversal(node.right, result)
+
+
+tree = BinaryTree()
+tree.insert(1)
+tree.insert(2)
+tree.insert(3)
+
+print("Inorder Traversal: ", tree.inoder_traversal())
