91d1ed00ffd04614d0473a6bcb8006cd4ccb65b1
Guilherme <guimars22@gmail.com>
2026-08-10 20:09:55 -0300

    #

--- Tests/Verzel/Lista.py
+++ Tests/Verzel/Lista.py
@@ -1,1 +1,26 @@
+numbers = [10, 5, 8, 20, 3, 20, 15]
 
+def AnalisarLista(numbers):
+    maiorNumero = 0
+    menorNumero = 0
+    soma = 0
+    mediaLista = 0
+    segundoMaiorNumero = 0
+
+    for i in range(len(numbers)):
+        if numbers[i] > maiorNumero:
+            maiorNumero = numbers[i]
+            menorNumero = i
+        if numbers[i] < maiorNumero:
+            segundoMaiorNumero = numbers[i]
+
+
+    mediaLista = sum(numbers) / len(numbers)
+
+    print("maior numero: ", maiorNumero)
+    print("menor numero: ", menorNumero)
+    print("média: ", mediaLista)
+    print("segundo maior número:" , segundoMaiorNumero)
+
+
+AnalisarLista(numbers)
