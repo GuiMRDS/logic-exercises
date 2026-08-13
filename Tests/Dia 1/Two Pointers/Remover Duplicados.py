d9bac5a64dd2d1cbca73b590338d6076fcd81495
Guilherme <guimars22@gmail.com>
2026-08-13 11:32:40 -0300

    #

--- Tests/Dia 1/Two Pointers/Remover Duplicados.py
+++ Tests/Dia 1/Two Pointers/Remover Duplicados.py
@@ -1,4 +1,20 @@
 nums = [1,1,2,2,3,4,4]
 
-[1,2,3,4]
+# Saída:
+# [1,2,3,4]
 
+def remove_duplicates(nums):
+    if not nums:
+        return 0
+
+    left = 0
+
+    for right in range(len(nums)):
+        if nums[right] != nums[left]:
+            left += 1
+            nums[left] = nums[right]
+
+    return nums[:left + 1]
+
+
+print(remove_duplicates(nums))
