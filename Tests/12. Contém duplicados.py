9815e219d2c61ea5348a816ba2cd8fbfbb0049bd
Guilherme <guimars22@gmail.com>
2026-08-17 18:58:08 -0300

    #

--- Tests/12. Contém duplicados.py
+++ Tests/12. Contém duplicados.py
@@ -4,3 +4,21 @@
 # [1, 2, 3, 1]
 # → true
 
+array = [1, 2, 3, 4]
+array2 = [1, 2, 3, 1]
+
+
+def HashMap(nums):
+    seen_numbers = set()
+
+    for n in nums:
+        if n in seen_numbers:
+            return True
+        seen_numbers.add(n)
+
+    return False
+
+
+
+print(HashMap(array))
+print(HashMap(array2))
