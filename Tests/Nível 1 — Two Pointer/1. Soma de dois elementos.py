15e3b90bdd3f73586789d5fa535cdc9e44bba474
Guilherme <guimars22@gmail.com>
2026-08-18 09:09:17 -0300

    #

---
+++ Tests/Nível 1 — Two Pointer/1. Soma de dois elementos.py
@@ -0,0 +1,27 @@
+array = [1, 2, 4, 6, 8, 9, 14]
+target = 10
+
+# Saída:
+# [2, 8]
+
+
+def TwoPointer(array, target):
+    left = 0
+    right = len(array)-1
+
+    while left < right:
+        soma = array[left] + array[right]
+
+        if soma == target:
+            return array[left], array[right]
+
+        elif soma < target:
+            left += 1
+
+        else:
+            right -= 1
+
+    return False
+
+
+print(TwoPointer(array, target))
