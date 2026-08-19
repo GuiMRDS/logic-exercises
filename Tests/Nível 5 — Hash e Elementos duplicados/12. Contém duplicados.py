b2cd13b0b73aff82121e4544c1444785d1e06732
Guilherme <guimars22@gmail.com>
2026-08-19 18:50:19 -0300

    #

--- Tests/Nível 5 — Hash e Elementos duplicados/12. Contém duplicados.py
+++ Tests/Nível 5 — Hash e Elementos duplicados/12. Contém duplicados.py
@@ -7,16 +7,3 @@
 array = [1, 2, 3, 4]
 array1 = [1, 2, 3, 1]
 
-def Hash(array):
-    visto = set()
-
-    for num in array:
-        if num in visto:
-            return True
-        visto.add(num)
-
-    return False
-
-
-print(Hash(array))
-print(Hash(array1))
--- Tests/Nível 5 — Hash e Elementos duplicados/13. Primeiro elemento duplicado.py
+++ Tests/Nível 5 — Hash e Elementos duplicados/13. Primeiro elemento duplicado.py
@@ -3,14 +3,3 @@ array = [2, 1, 3, 5, 3, 2]
 # Saída:
 # 3
 
-def hash(array):
-    visto = set()
-
-    for elemento in array:
-        if elemento in visto:
-            return elemento
-        visto.add(elemento)
-
-    return -1
-
-print(hash(array))
