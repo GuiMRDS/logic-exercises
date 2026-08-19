a6d9d790dfe0661a9c7ecb332c527475f603c5f4
Guilherme <guimars22@gmail.com>
2026-08-19 18:27:07 -0300

    #

--- Tests/Nível 1 — Two Pointer/1. Soma de dois elementos.py
+++ Tests/Nível 1 — Two Pointer/1. Soma de dois elementos.py
@@ -5,23 +5,3 @@ target = 10
 # [2, 8]
 
 
-def TwoPointer(array, target):
-    left = 0
-    right = len(array)-1
-
-    while left < right:
-        soma = array[left] + array[right]
-
-        if soma == target:
-            return array[left], array[right]
-
-        elif soma < target:
-            left += 1
-
-        else:
-            right -= 1
-
-    return False
-
-
-print(TwoPointer(array, target))
--- Tests/Nível 1 — Two Pointer/3. Remover duplicados.py
+++ Tests/Nível 1 — Two Pointer/3. Remover duplicados.py
@@ -4,20 +4,3 @@
 # Saída:
 # [1, 2, 3, 4]
 
-
-def TwoPointer(arr):
-    if not arr:
-        return 0
-
-    left = 0
-    for right in range(len(arr)):
-        if arr[right] != arr[left]:
-            left += 1
-            arr[left] =  arr[left]
-
-    return left + 1
-
-
-array = [1, 1, 2, 2, 3, 4, 4]
-quantidade = TwoPointer(array)
-print(array[:quantidade])
