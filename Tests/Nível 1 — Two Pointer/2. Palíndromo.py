1562a011ff1eaeb578c14d3e80dae7ef583104a6
Guilherme <guimars22@gmail.com>
2026-08-19 18:31:20 -0300

    #

--- Tests/Nível 1 — Two Pointer/2. Palíndromo.py
+++ Tests/Nível 1 — Two Pointer/2. Palíndromo.py
@@ -5,3 +5,16 @@
 # Saída: false
 
 
+def TwoPointer(palavra):
+    left = 0
+    right = len(palavra)-1
+
+    while left < right:
+        if palavra[left] == palavra[right]:
+            return True
+
+        return False
+
+palavra = input("Digite uma palavra: ")
+print(TwoPointer(palavra))
+
