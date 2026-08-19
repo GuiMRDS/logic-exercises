1d6563e366b3ed6b75f72f3c126712835c71ed9a
Guilherme <guimars22@gmail.com>
2026-08-19 18:48:01 -0300

    #

--- Tests/Nível 2 — Binary Search/6. Encontrar posição de inserção.py
+++ Tests/Nível 2 — Binary Search/6. Encontrar posição de inserção.py
@@ -4,4 +4,20 @@ target = 4
 # Saída:
 # 2
 
+def binary_search(array, target):
+    low = 0
+    high = len(array) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
+
+        if array[mid] >= target:
+            return mid
+
+        else:
+            low = mid + 1
+
+    return -1
+
+
+print(binary_search(array, target))
