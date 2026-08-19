b60863bc1c5a2e3e568738c7e4378c33c1e9eb88
Guilherme <guimars22@gmail.com>
2026-08-19 18:37:15 -0300

    #

--- Tests/Nível 2 — Binary Search/4. Buscar elemento.py
+++ Tests/Nível 2 — Binary Search/4. Buscar elemento.py
@@ -4,24 +4,3 @@ target = 7
 # Saída:
 # 3
 
-
-def BinarySearch(array, target):
-    low = 0
-    high = len(array) - 1
-
-    while low <= high:
-        mid = (low + high) // 2
-
-        if array[mid] == target:
-            return mid
-
-        elif array[mid] > target:
-            high = mid - 1
-
-        else:
-            low = mid + 1
-
-    return -1
-
-
-print(BinarySearch(array, target))
--- Tests/Nível 2 — Binary Search/5. Primeiro elemento maior ou igual.py
+++ Tests/Nível 2 — Binary Search/5. Primeiro elemento maior ou igual.py
@@ -5,23 +5,3 @@ target = 6
 # 7
 
 
-def BinarySearch(array, target):
-    low = 0
-    high = len(array) - 1
-
-    while low <= high:
-        mid = (low + high) // 2
-
-        if array[mid] >= target:
-            return array[mid]
-
-        elif array[mid] < target:
-            low = mid + 1
-
-        else:
-            high = mid - 1
-
-    return -1
-
-
-print(BinarySearch(array, target))
