41a03575dcf3fecf269aa5b8ac08ab8a8f573b04
Guilherme <guimars22@gmail.com>
2026-08-19 11:43:47 -0300

    #

--- Tests/Nível 8 — Linked List/DoublyLinkedList.py
+++ Tests/Nível 8 — Linked List/DoublyLinkedList.py
@@ -9,3 +9,52 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
 
+    def add_to_front(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        if self.head:
+            self.head.prev = new_node
+        else:
+            self.tail = new_node
+        self.head = new_node
+
+
+    def add_to_end(self, value):
+        new_node = Node(value)
+        new_node.prev = self.tail
+        if self.tail:
+            self.tail.next = new_node
+        else:
+            self.head = new_node
+        self.tail = new_node
+
+
+    def remove_from_front(self, value):
+        if not self.head:
+            return None
+
+        removed_node = self.head.value
+
+        self.head = self.head.next
+        if self.head:
+            self.head.prev = None
+        else:
+            self.tail = None
+
+        return removed_node
+
+
+    def remove_from_end(self, value):
+        if not self.tail:
+            return None
+
+        removed_node = self.tail.value
+
+        self.tail = self.tail.prev
+        if self.tail:
+            self.tail.next = None
+        else:
+            self.head = None
+
+        return removed_node
+
