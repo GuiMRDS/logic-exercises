4956b5391d86f95d0f4706092af3f1e862e8f288
Guilherme <guimars22@gmail.com>
2026-08-19 19:12:02 -0300

    #

--- Tests/Nível 9 — Sorting/25. Bubble Sort.py
+++ Tests/Nível 9 — Sorting/25. Bubble Sort.py
@@ -2,15 +2,16 @@
 # → [2, 3, 4, 5, 8]
 
 
-def bubble_sort(array):
-    size = len(array)
+def bubble_sort(arr):
+    tamanho = len(arr)
 
-    for _ in array:
-        print(array)
-        for i in range(size - 1):
-            if array[i] > array[i + 1]:
-                array[i+1], array[i] = array[i], array[i+1]
+    for _ in arr:
+        print(arr)
+        for i in range(tamanho-1):
+            if arr[i] > arr[i+1]:
+                arr[i + 1], arr[i] = arr[i], arr[i + 1]
 
 
 
-bubble_sort([5, 3, 8, 4, 2])
+print(bubble_sort([5,4,3,2,1]))
+print(bubble_sort([5, 3, 8, 4, 2]))
--- Tests/Nível 9 — Sorting/26. Selection Sort.py
+++ Tests/Nível 9 — Sorting/26. Selection Sort.py
@@ -2,14 +2,3 @@
 # → [11, 12, 22, 25, 64]
 
 
-def selection_sort(array):
-    size = len(array)
-
-    for _ in array:
-        print(array)
-        for i in range(size - 1):
-            if array[i] > array[i + 1]:
-                array[i + 1], array[i] = array[i], array[i + 1]
-
-
-selection_sort([64, 25, 12, 22, 11])
