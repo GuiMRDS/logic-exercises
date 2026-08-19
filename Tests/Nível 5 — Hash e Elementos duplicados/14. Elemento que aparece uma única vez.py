8cdf9098439611cb04b731ba7380fba1b01fd330
Guilherme <guimars22@gmail.com>
2026-08-19 19:08:13 -0300

    #

--- Tests/Nível 5 — Hash e Elementos duplicados/14. Elemento que aparece uma única vez.py
+++ Tests/Nível 5 — Hash e Elementos duplicados/14. Elemento que aparece uma única vez.py
@@ -3,3 +3,16 @@ array = [4, 1, 2, 1, 2]
 # Saída:
 # 4
 
+def Hash(array):
+    visto = set()
+
+    for num in array:
+        if not num in visto:
+            return num
+
+        visto.add(num)
+
+    return False
+
+
+print(hash(array))
