66589a4c143c039693625c8bf65266e769af2f86
Guilherme <guimars22@gmail.com>
2026-08-19 10:56:54 -0300

    #

---
+++ Tests/Nível 9 — Sorting/26. Selection Sort.py
@@ -0,0 +1,15 @@
+# [64, 25, 12, 22, 11]
+# → [11, 12, 22, 25, 64]
+
+
+def selection_sort(array):
+    size = len(array)
+
+    for _ in range(size - 1):
+        print(array)
+        for i in range(size - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+
+
+selection_sort([64, 25, 12, 22, 11])
