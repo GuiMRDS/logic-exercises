9603ac29a2894a729de0bebebb9552532be3cd46
Guilherme <guimars22@gmail.com>
2026-08-20 18:30:18 -0300

    #

--- Tests/Nível 6 — HashMap/15. Two Sum.py
+++ Tests/Nível 6 — HashMap/15. Two Sum.py
@@ -6,8 +6,22 @@ target = 9
 
 
 def twoSum(array, target):
+    left = 0
+    right = len(array) - 1
 
+    while left < right:
+        mid = (left + right) // 2
 
+        if array[mid] == target:
+            return [array[left], array[mid]]
+
+        elif array[mid] > target:
+            right = mid - 1
+
+        else:
+            left = mid + 1
+
+    return False
 
 
 print(twoSum(array, target))
