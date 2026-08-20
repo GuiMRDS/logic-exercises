2deb107c6dd6f7bc761f9893397232f22bbf8707
Guilherme <guimars22@gmail.com>
2026-08-20 18:19:30 -0300

    #

--- Tests/Nível 1 — Two Pointer/3. Remover duplicados.py
+++ Tests/Nível 1 — Two Pointer/3. Remover duplicados.py
@@ -5,3 +5,22 @@
 # [1, 2, 3, 4]
 
 
+def TwoPointer(arr):
+    left = 0
+    right = 1
+    size = len(arr)
+
+    while right < size:
+        if arr[left] == arr[right]:
+            arr[left] = arr[right]
+            left += 1
+            right += 1
+
+        left += 1
+        right += 1
+
+    return arr
+
+
+
+print(TwoPointer([1, 1, 2, 2, 3, 4, 4]))
