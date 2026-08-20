882926ce6a6b19109421589eb57b83af394633db
Guilherme <guimars22@gmail.com>
2026-08-20 10:24:37 -0300

    #

--- Tests/Nível 7 — Subarrays/20. Maior subarray com soma zero.py
+++ Tests/Nível 7 — Subarrays/20. Maior subarray com soma zero.py
@@ -1,4 +1,29 @@
 array = [15, -2, 2, -8, 1, 7, 10, 23]
+k = 0
 
 # Saída:
 # 5
+
+def SubarraySum(array, k):
+    prefix_sum = 0
+    maior = 0
+
+    indices = {0: -1}
+
+    for i, num in enumerate(array):
+        prefix_sum += num
+
+        if prefix_sum - k in indices:
+            tamanho = i - indices[prefix_sum - k]
+            maior = max(maior, tamanho)
+
+        if prefix_sum not in indices:
+            indices[prefix_sum] = i
+
+    return maior
+
+
+print(SubarraySum(array, 0))
+
+
+print(SubarraySum(array, k))
