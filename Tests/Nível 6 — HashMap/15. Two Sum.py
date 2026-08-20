6f6b78428b8350e4eacc9e227113bd29f41d9163
Guilherme <guimars22@gmail.com>
2026-08-20 09:47:22 -0300

    #

--- Tests/Nível 6 — HashMap/15. Two Sum.py
+++ Tests/Nível 6 — HashMap/15. Two Sum.py
@@ -4,3 +4,25 @@ target = 9
 # Saída:
 # [0, 1]
 
+
+def twoSum(array, target):
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+        soma = array[left] + array[right]
+
+        if soma == target:
+            return [left, right]
+
+        elif soma < target:
+            left += 1
+
+        else:
+            right -= 1
+
+    return None
+
+
+
+print(twoSum(array, target))
