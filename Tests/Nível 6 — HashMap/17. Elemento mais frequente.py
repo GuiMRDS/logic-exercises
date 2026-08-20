28a3614317e3951a9e3a3e1922d31439b1c1d4aa
Guilherme <guimars22@gmail.com>
2026-08-20 18:31:39 -0300

    #

--- Tests/Nível 6 — HashMap/17. Elemento mais frequente.py
+++ Tests/Nível 6 — HashMap/17. Elemento mais frequente.py
@@ -5,7 +5,13 @@ array = [1, 3, 2, 1, 4, 1, 3]
 
 
 def hashMap(array):
+    vistos= set()
 
+    for elemento in array:
+        if elemento in vistos:
+            return elemento
+
+        vistos.add(elemento)
 
 
 print(hashMap(array))
