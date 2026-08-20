a6628af4f7b9bc91dce233399bd262447c912212
Guilherme <guimars22@gmail.com>
2026-08-20 10:14:30 -0300

    #

--- Tests/Nível 7 — Subarrays/18. Maior soma de subarray.py
+++ Tests/Nível 7 — Subarrays/18. Maior soma de subarray.py
@@ -2,3 +2,16 @@ array = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
 # Saída:
 # 6
+
+def MaiorSubarrays(array):
+    atual = array[0]
+    maior = array[0]
+
+    for i in range(1, len(array)):
+        atual = max(array[i], atual + array[i])
+        maior = max(maior, atual)
+
+    return maior
+
+
+print(MaiorSubarrays(array))
