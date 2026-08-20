b77f310637cf70fa530e9935692ddc0508290120
Guilherme <guimars22@gmail.com>
2026-08-20 10:10:33 -0300

    #

--- Tests/Nível 3 — Sliding Window/9. Menor subarray com soma ≥ target.py
+++ Tests/Nível 3 — Sliding Window/9. Menor subarray com soma ≥ target.py
@@ -3,3 +3,23 @@ target = 7
 
 # Saída:
 # 2
+
+def SlidingWindow(array, target):
+    esquerda = 0
+    soma = 0
+    menor = float("inf")
+
+    for direita in range(len(array)):
+        soma += array[direita]
+
+        while soma >= target:
+            menor = min(menor, direita - esquerda + 1)
+
+            soma -= array[esquerda]
+            esquerda += 1
+
+    return menor if menor != float("inf") else 0
+
+
+
+print(SlidingWindow(array, target))
