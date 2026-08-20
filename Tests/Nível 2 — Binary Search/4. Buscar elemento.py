fba4b6c15cc026fbd4bcf3afec908b1f70268a3d
Guilherme <guimars22@gmail.com>
2026-08-20 18:20:16 -0300

    #

--- Tests/Nível 2 — Binary Search/4. Buscar elemento.py
+++ Tests/Nível 2 — Binary Search/4. Buscar elemento.py
@@ -5,8 +5,22 @@ target = 7
 # 3
 
 def BinarySearch(array, target):
+    left = 0
+    right = len(array) - 1
 
+    while left <= right:
+        mid = (left + right) // 2
 
+        if array[mid] == target:
+            return mid
+
+        elif array[mid] > target:
+            right = mid - 1
+
+        else:
+            left = mid + 1
+
+    return -1
 
 
 print(BinarySearch(array, target))
