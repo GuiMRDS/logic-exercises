f7fc29168320ad7f679bbf9108bdb9f610ec4b07
Guilherme <guimars22@gmail.com>
2026-08-20 09:50:26 -0300

    #

--- Tests/Nível 6 — HashMap/16. Frequência dos elementos.py
+++ Tests/Nível 6 — HashMap/16. Frequência dos elementos.py
@@ -4,3 +4,16 @@ array = [1, 2, 2, 3, 1, 1]
 # 1 → 3
 # 2 → 2
 # 3 → 1
+
+
+def hashMap(array):
+    hashMap = set()
+
+    for elemento in array:
+        hashMap.add(elemento)
+
+    return hashMap
+
+
+
+print(hashMap(array))
