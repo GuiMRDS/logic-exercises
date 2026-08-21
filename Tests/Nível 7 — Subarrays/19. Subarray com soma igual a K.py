3dbdef5223ebcc25b9bc320a4a1f438de6bcee4d
Guilherme <guimars22@gmail.com>
2026-08-21 10:43:50 -0300

    #

--- Tests/Nível 7 — Subarrays/19. Subarray com soma igual a K.py
+++ Tests/Nível 7 — Subarrays/19. Subarray com soma igual a K.py
@@ -4,4 +4,15 @@ k = 2
 # Saída:
 # 2
 
+def MaiorSubarrays(array, k):
+    soma = sum(array[:k])
+    maior = soma
 
+    for direita in range(k, len(array)):
+        soma = soma - array[direita - k] + array[direita]
+        maior = max(soma, maior)
+
+    return maior
+
+
+print(MaiorSubarrays(array, k))
