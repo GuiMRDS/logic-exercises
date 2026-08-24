b9c759aeada4da439c1af0d7219092e7bf89b790
Guilherme <guimars22@gmail.com>
2026-08-24 11:01:21 -0300

    #

--- Tests/Nível 1 — Two Pointer/3. Remover duplicados.py
+++ Tests/Nível 1 — Two Pointer/3. Remover duplicados.py
@@ -5,3 +5,16 @@
 # [1, 2, 3, 4]
 
 
+def TwoPointer(array):
+    visto = set()
+
+    for num in array:
+        if num in visto:
+            visto.remove(num)
+
+        visto.add(num)
+
+    return visto
+
+
+print(TwoPointer([1, 1, 2, 2, 3, 4, 4]))
