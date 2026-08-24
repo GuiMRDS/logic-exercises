d337fdfe7ab048617d3c1f5d5d568daff8314d8e
Guilherme <guimars22@gmail.com>
2026-08-24 11:16:50 -0300

    #

--- Tests/Nível 9 — Sorting/25. Bubble Sort.py
+++ Tests/Nível 9 — Sorting/25. Bubble Sort.py
@@ -2,16 +2,4 @@
 # → [2, 3, 4, 5, 8]
 
 
-def bubble_sort(arr):
-    size = len(arr)
 
-    for _ in range(size-1):
-        print(arr)
-        for index in range(size-1):
-            if arr[index] > arr[index+1]:
-                arr[index], arr[index+1] = arr[index+1], arr[index]
-
-
-
-print(bubble_sort([5,4,3,2,1]))
-print(bubble_sort([5, 3, 8, 4, 2]))
