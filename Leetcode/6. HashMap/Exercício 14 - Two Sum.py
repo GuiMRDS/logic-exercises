a6b9899fb2b1e69294421897b37b058c9da838b2
Guilherme <guimars22@gmail.com>
2026-08-26 11:39:07 -0300

    #

--- Leetcode/6. HashMap/Exercício 14 - Two Sum.py
+++ Leetcode/6. HashMap/Exercício 14 - Two Sum.py
@@ -5,3 +5,23 @@ target = 9
 # Resposta:
 # [0,1]
 
+
+def containsDuplicate(nums):
+    left, right = 0, len(nums)-1
+
+    while left < right:
+        sum = nums[left] + nums[right]
+
+        if sum == target:
+            return True
+
+        elif sum < target:
+            left += 1
+
+        else:
+            right -= 1
+
+    return False
+
+
+print(containsDuplicate(nums))
