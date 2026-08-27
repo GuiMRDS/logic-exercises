58ae03735b8a6d8a43d7e4a0ada3058c84a5a8bb
Guilherme <guimars22@gmail.com>
2026-08-27 11:36:53 -0300

    #

--- Tests/Nível 2 — Binary Search/5. Primeiro elemento maior ou igual.py
+++ Tests/Nível 2 — Binary Search/5. Primeiro elemento maior ou igual.py
@@ -4,4 +4,20 @@ target = 6
 # Saída:
 # 7
 
+def binary_search(array, target):
+    low = 0
+    high = len(array) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
+
+        if array[mid] >= target:
+            return array[mid]
+
+        else:
+            low = mid + 1
+
+    return -1
+
+
+print(binary_search(array, target))
