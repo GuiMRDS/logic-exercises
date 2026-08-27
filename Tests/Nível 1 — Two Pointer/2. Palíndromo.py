91704205cbcc825d7b7c25cc81884bed2e26c20b
Guilherme <guimars22@gmail.com>
2026-08-27 11:29:49 -0300

    #

--- Tests/Nível 1 — Two Pointer/2. Palíndromo.py
+++ Tests/Nível 1 — Two Pointer/2. Palíndromo.py
@@ -5,3 +5,19 @@
 # Saída: false
 
 
+def TwoPointer(pal):
+    left = 0
+    right = len(pal) - 1
+
+    while left < right:
+        if pal[left] == pal[right]:
+            return True
+
+        left += 1
+        right -= 1
+
+    return False
+
+
+print(TwoPointer('racecar'))
+print(TwoPointer('hello'))
