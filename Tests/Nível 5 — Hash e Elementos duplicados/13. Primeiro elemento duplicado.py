6a0ea521219796ed9d0c4ed22e4d220d0a8ff278
Guilherme <guimars22@gmail.com>
2026-08-27 11:49:54 -0300

    #

--- Tests/Nível 5 — Hash e Elementos duplicados/13. Primeiro elemento duplicado.py
+++ Tests/Nível 5 — Hash e Elementos duplicados/13. Primeiro elemento duplicado.py
@@ -3,3 +3,16 @@
 # Saída:
 # 3
 
+def Hash(array):
+    visto = set()
+
+    for elemento in array:
+        if elemento not in visto:
+            return array[elemento]
+
+        visto.add(elemento)
+
+    return None
+
+
+print(Hash([2, 1, 3, 5, 3, 2]))
