7e835a509244f188a04483eb70fb58f0f4d76763
Guilherme <guimars22@gmail.com>
2026-08-27 11:52:21 -0300

    #

--- Tests/Nível 6 — HashMap/15. Two Sum.py
+++ Tests/Nível 6 — HashMap/15. Two Sum.py
@@ -5,21 +5,3 @@ target = 9
 # [0, 1]
 
 
-def Hash_Two_Sum(nums, target):
-    left = 0
-    right = len(nums) - 1
-
-    while left < right:
-        sum = nums[left] + nums[right]
-
-        if sum == target:
-            return [left, right]
-
-        elif sum < target:
-            left += 1
-
-        else:
-            right -= 1
-
-
-print(Hash_Two_Sum(array, target))
