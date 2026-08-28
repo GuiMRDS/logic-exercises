1e0f3a692a2c12559b37644ea1bdee417cd3f602
Guilherme <guimars22@gmail.com>
2026-08-28 10:48:07 -0300

    #

--- Leetcode/1. Two Pointers/Exercício 1 - Two Sum Ordenado.py
+++ Leetcode/1. Two Pointers/Exercício 1 - Two Sum Ordenado.py
@@ -5,3 +5,22 @@ target = 8
 # [2,3]
 
 
+def TwoPointer(nums):
+    left, right = 0, len(nums)-1
+
+    while left <= right:
+        soma=nums[left] + nums[right]
+
+        if soma == target:
+            return [left, right]
+
+        elif soma < target:
+            left += 1
+
+        else:
+            right -= 1
+
+    return None
+
+
+print(TwoPointer(nums))
