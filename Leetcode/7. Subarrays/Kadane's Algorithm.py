b8e0375b15e8d95d7b39b10481bb7349684b884a
Guilherme <guimars22@gmail.com>
2026-08-28 11:00:40 -0300

    #

---
+++ Leetcode/7. Subarrays/Kadane's Algorithm.py
@@ -0,0 +1,20 @@
+array = [-2,1,-3,4,-1,2,1,-5,4]
+
+# Resposta:
+# 6
+
+def maxSubarraySum(arr):
+    res = arr[0]
+
+    for i in range(len(arr)):
+        currSum = 0
+
+        for j in range(i, len(arr)):
+            currSum = currSum + arr[j]
+
+            res = max(res, currSum)
+
+    return res
+
+
+print(maxSubarraySum(array))
