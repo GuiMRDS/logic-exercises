f92e1c9ae57bbbb36372b035387ff5b48342dc73
Guilherme <guimars22@gmail.com>
2026-08-31 11:34:14 -0300

    #

--- Leetcode/5. HashSet Duplicados/Exercício 11 - Contains Duplicate.py
+++ Leetcode/5. HashSet Duplicados/Exercício 11 - Contains Duplicate.py
@@ -3,3 +3,16 @@
 # true
 
 
+def hashSet(nums):
+    visted = set()
+
+    for num in nums:
+        if num in visted:
+            return True
+
+        visted.add(num)
+
+    return False
+
+
+print(hashSet([1,2,3,1]))
