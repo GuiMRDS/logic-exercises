485680e4c5bde8d03c14e422965aaf8725f0bbba
Guilherme <guimars22@gmail.com>
2026-08-31 11:44:15 -0300

    #

--- Leetcode/5. HashSet Duplicados/Exercício 12 - Missing Number.py
+++ Leetcode/5. HashSet Duplicados/Exercício 12 - Missing Number.py
@@ -4,15 +4,11 @@
 # 2
 
 def hashSet(nums):
-    visted = set()
+    visited = set()
+    size = len(nums)
 
-    for num in nums:
-        if num in visted:
-            return visted
+    
 
-        visted.add(num)
-
-    return len(visted)
 
 
 print(hashSet([3,0,1]))
