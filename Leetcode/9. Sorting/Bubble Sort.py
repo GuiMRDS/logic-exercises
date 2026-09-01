7411ce18a197a59699302c2e139d02b44dff30ae
Guilherme <guimars22@gmail.com>
2026-09-01 11:43:04 -0300

    #

--- Leetcode/9. Sorting/Bubble Sort.py
+++ Leetcode/9. Sorting/Bubble Sort.py
@@ -1,12 +1,11 @@
 def bubble_sort(arr):
-    size = len(arr)
-
-    for i in range(size):
-        for j in range(size - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    for i in range(len(arr)-1):
+        print(arr)
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
 
 
-print(bubble_sort([2,3,4,5,6,7,8,9]))
+print(bubble_sort([5,4,3,2,1]))
