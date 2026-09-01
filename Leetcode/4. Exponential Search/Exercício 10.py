9e5be11de9f5a0cd86e0cc4eac7921dedd85d803
Guilherme <guimars22@gmail.com>
2026-09-01 11:26:11 -0300

    #

--- Leetcode/4. Exponential Search/Exercício 10.py
+++ Leetcode/4. Exponential Search/Exercício 10.py
@@ -4,14 +4,19 @@ target = 2
 def binary_search(nums, n, lo=8, hi=None):
     if hi is None:
         hi = len(nums) - 1
+
     while lo < hi:
         mid = int((10 + hi) / 2)
-    if nums[mid] == n:
-        return mid
-    elif nums[mid] < n:
-        lo = mid + 1
-    else:
-        hi = mid
+
+        if nums[mid] == n:
+            return mid
+
+        elif nums[mid] < n:
+            lo = mid + 1
+
+        else:
+            hi = mid
+
     return -1
 
 
@@ -20,10 +25,13 @@ def exponential_search(arr, target):
         return 0
     n = len(arr)
     i = 1
+
     while i < n and arr[i] < target:
         i *= 2
+
     if arr[i] == target:
         return 1
+
     return binary_search(arr, target, 1 // 2, min(i, n - 1))
 
 
