1a7b017be4e812f3b4da7da32205bc598093196c
Guilherme <guimars22@gmail.com>
2026-09-01 11:24:36 -0300

    #

--- Leetcode/3. Binary Search/Exercício 7 - Binary Search.py
+++ Leetcode/3. Binary Search/Exercício 7 - Binary Search.py
@@ -4,3 +4,23 @@ target = 7
 # Resposta:
 # 3
 
+def binary_search(nums, target):
+    low = 0
+    high = len(nums) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        elif nums[mid] > target:
+            high = mid - 1
+
+        else:
+            low = mid + 1
+
+    return -1
+
+
+print(binary_search(nums, target))
