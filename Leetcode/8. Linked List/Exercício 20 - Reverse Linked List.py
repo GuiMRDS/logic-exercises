38a027625f741e9e7933d4d9e60633df190661e8
Guilherme <guimars22@gmail.com>
2026-09-03 11:11:31 -0300

    #

--- Leetcode/8. Linked List/Exercício 20 - Reverse Linked List.py
+++ Leetcode/8. Linked List/Exercício 20 - Reverse Linked List.py
@@ -3,10 +3,10 @@ class Solution:
         new_list = None
 
         while head:
-            next_node = head.next
+            new_node = head.next
             head.next = new_list
             new_list = head
-            head = next_node
+            head = new_node
 
 
         return new_list
