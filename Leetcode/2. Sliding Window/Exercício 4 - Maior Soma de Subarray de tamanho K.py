042327787084ca275f4c921c4a9c030def4625b8
Guilherme <guimars22@gmail.com>
2026-09-03 10:46:05 -0300

    #

--- Leetcode/2. Sliding Window/Exercício 4 - Maior Soma de Subarray de tamanho K.py
+++ Leetcode/2. Sliding Window/Exercício 4 - Maior Soma de Subarray de tamanho K.py
@@ -3,29 +3,3 @@ k = 3
 
 # Resposta:
 # 9
-
-
-def sliding_window(nums, k):
-    esquerda = 0
-    direita = k - 1
-
-    soma_atual = sum(nums[:k])
-    maior_soma = soma_atual
-
-    while direita < len(nums) - 1:
-        direita += 1
-
-        # Adiciona o novo elemento que entrou na janela
-        soma_atual += nums[direita]
-
-        # Remove o elemento que saiu da janela
-        soma_atual -= nums[esquerda]
-
-        esquerda += 1
-
-        maior_soma = max(maior_soma, soma_atual)
-
-    return maior_soma
-
-
-print(sliding_window(nums, k))
