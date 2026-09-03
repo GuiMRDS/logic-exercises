a196c43be00a7635e52353260d54c48bd100b754
Guilherme <guimars22@gmail.com>
2026-09-03 11:17:48 -0300

    #

--- Leetcode/9. Sorting/Selection Sort.py
+++ Leetcode/9. Sorting/Selection Sort.py
@@ -1,7 +1,7 @@
 def selection_sort(arr):
     n = len(arr)
 
-    for i in range(len(arr)):
+    for i in range(n):
         min_idx = i
 
         for j in range(i+1, n):
@@ -11,7 +11,6 @@ def selection_sort(arr):
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
     return arr
-
 
 
 print(selection_sort([5,4,3,2,1]))
