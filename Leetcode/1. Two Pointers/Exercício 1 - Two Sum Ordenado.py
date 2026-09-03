20a750a0f75a44f2abd54838d5684079343f69c1
Guilherme <guimars22@gmail.com>
2026-09-03 10:44:52 -0300

    #

--- Leetcode/1. Two Pointers/Exercício 1 - Two Sum Ordenado.py
+++ Leetcode/1. Two Pointers/Exercício 1 - Two Sum Ordenado.py
@@ -1,6 +1,18 @@
-nums = [3,2,4]
+nums = [0,3,2,4]
 target = 6
 
 # Resposta:
 # [2,3]
 
+def twoSum(nums, target):
+    size = len(nums)
+
+    for i in range(size):
+        for j in range(i+1, size):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+    return None
+
+
+print(twoSum(nums, target))
