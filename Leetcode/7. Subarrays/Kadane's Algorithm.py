e1875a0ca627142d47d95315cc3172c46f1f8ab2
Guilherme <guimars22@gmail.com>
2026-09-03 11:02:22 -0300

    #

--- Leetcode/7. Subarrays/Kadane's Algorithm.py
+++ Leetcode/7. Subarrays/Kadane's Algorithm.py
@@ -17,4 +17,18 @@ def kadaneSum(arr):
     return res
 
 
+def algorithmKadaneSum(arr):
+    res = arr[0]
+
+    for i in range(1, len(arr)):
+        currSum = 0
+
+        for j in range(i, len(arr)):
+            currSum = currSum + arr[j]
+
+            res = max(res, currSum)
+
+    return res
+
+
 print(kadaneSum(array))
