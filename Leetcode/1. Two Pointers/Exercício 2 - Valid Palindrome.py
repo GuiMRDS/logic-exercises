1ecaef05738ad98167043081a2eefef3b3b017e7
Guilherme <guimars22@gmail.com>
2026-09-04 10:37:09 -0300

    #

--- Leetcode/1. Two Pointers/Exercício 2 - Valid Palindrome.py
+++ Leetcode/1. Two Pointers/Exercício 2 - Valid Palindrome.py
@@ -1,3 +1,17 @@
 frase = "A man a plan a canal Panama"
 # true
 
+def validPalindrome(str):
+    left, right = 0, len(str) - 1
+
+    while left < right:
+        if str[left] == str[right]:
+            return True
+
+        left += 1
+        right -= 1
+
+    return False
+
+
+print(validPalindrome(frase))
