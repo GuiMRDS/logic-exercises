45e31d8401a8f5d8471668132693ea1af9fa276c
Guilherme <guimars22@gmail.com>
2026-09-04 10:30:43 -0300

    #

--- Leetcode/1. Two Pointers/Exercício 1 - Two Sum Ordenado.py
+++ Leetcode/1. Two Pointers/Exercício 1 - Two Sum Ordenado.py
@@ -4,15 +4,3 @@ target = 6
 # Resposta:
 # [2,3]
 
-def twoSum(nums, target):
-    size = len(nums)
-
-    for i in range(size):
-        for j in range(i+1, size):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-
-    return None
-
-
-print(twoSum(nums, target))
--- Leetcode/1. Two Pointers/Exercício 2 - Valid Palindrome.py
+++ Leetcode/1. Two Pointers/Exercício 2 - Valid Palindrome.py
@@ -1,17 +1,3 @@
 frase = "A man a plan a canal Panama"
 # true
 
-def isPalindrome(s):
-    left, right = 0, len(s) - 1
-
-    while left < right:
-        if s[left] == s[right]:
-            return True
-
-        left += 1
-        right -= 1
-
-    return False
-
-
-print(isPalindrome(frase))
