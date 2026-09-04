f1dcf302357518b1b3d80a989a8e22091860effe
Guilherme <guimars22@gmail.com>
2026-09-04 11:17:08 -0300

    #

--- Leetcode/10. Pilhas (Stacks)/Implemente uma stack.py
+++ Leetcode/10. Pilhas (Stacks)/Implemente uma stack.py
@@ -23,3 +23,37 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+
+
+class StackPointer:
+    def __init__(self, max_length=1000):
+        self.items = [0] * max_length
+        self.max_length = max_length
+        self.pointer = 0
+
+
+    def push(self, item):
+        if self.pointer >= self.max_length:
+            raise IndexError("Stack overflow")
+
+        self.items[self.pointer] = item
+        self.pointer += 1
+
+
+    def pop(self):
+        if self.pointer == 0:
+            raise IndexError("Emply Stack")
+
+        self.pointer -= 1
+        return self.items[self.pointer]
+
+
+    def peek(self):
+        if self.pointer == 0:
+            raise IndexError("Emply Stack")
+
+        return self.items[self.pointer - 1]
+
+    def size(self):
+        return self.pointer
