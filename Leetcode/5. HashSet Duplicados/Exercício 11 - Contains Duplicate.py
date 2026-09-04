6cdc716f2c1c9bee375b187387bf8270110d1c0c
Guilherme <guimars22@gmail.com>
2026-09-04 10:43:57 -0300

    #

--- Leetcode/5. HashSet Duplicados/Exercício 11 - Contains Duplicate.py
+++ Leetcode/5. HashSet Duplicados/Exercício 11 - Contains Duplicate.py
@@ -2,17 +2,3 @@
 
 # true
 
-def HashSetDuplicados(arr):
-    visited = set()
-
-    for num in arr:
-        if num in visited:
-            return True
-
-        visited.add(num)
-
-    return False
-
-
-print(HashSetDuplicados([1,2,3,4,5]))
-print(HashSetDuplicados([1,2,3,1]))
--- Leetcode/5. HashSet Duplicados/Exercício 12 - Missing Number.py
+++ Leetcode/5. HashSet Duplicados/Exercício 12 - Missing Number.py
@@ -3,3 +3,4 @@
 # Resposta:
 # 2
 
+
