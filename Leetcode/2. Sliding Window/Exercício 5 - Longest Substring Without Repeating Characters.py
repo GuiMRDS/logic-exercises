c2a75fc5c2ab5576ddd215e7932172b32f7f2412
Guilherme <guimars22@gmail.com>
2026-09-09 11:47:46 -0300

    #

--- Leetcode/2. Sliding Window/Exercício 5 - Longest Substring Without Repeating Characters.py
+++ Leetcode/2. Sliding Window/Exercício 5 - Longest Substring Without Repeating Characters.py
@@ -23,5 +23,22 @@ def lengthOfLongestSubstring(string):
     return ans
 
 
+def lengthOfLongestSubstring_1(string):
+    left, ans = 0, 0
+    counter = {}
+
+    for right in range(len(string)):
+        counter[string[right]] = counter.get(string[right], 0) + 1
+
+        while counter[string[right]] > 1:
+            counter[string[right]] -= 1
+            left += 1
+
+        ans = max(ans, right - left + 1)
+
+    return ans
+
+
+
 
 print(lengthOfLongestSubstring("abcabcbb"))
