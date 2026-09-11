d2445bb5b65309fab2238e7272b7b9e435bffae1
Guilherme <guimars22@gmail.com>
2026-09-11 11:45:37 -0300

    #

--- Leetcode/6. HashMap/Exercício 14 - Two Sum.py
+++ Leetcode/6. HashMap/Exercício 14 - Two Sum.py
@@ -5,19 +5,3 @@ target = 9
 # [0,1]
 
 
-def twoSumHashMap(nums, target):
-    hashMap = {}
-
-    for i in range(len(nums)):
-        hashMap[nums[i]] = i
-
-        for j in range(len(nums)):
-            if target - nums[j] in hashMap:
-                return [hashMap[target - nums[j]], j]
-            else:
-                hashMap[nums[j]] = i
-
-    return False
-
-
-print(twoSumHashMap(nums, target))
