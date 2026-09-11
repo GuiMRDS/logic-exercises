22e41d74bdaece02a49cf1b85eec5a0e8a1a3089
Guilherme <guimars22@gmail.com>
2026-09-11 19:15:00 -0300

    #

--- Python/Flask_App/main.py
+++ Python/Flask_App/main.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     nome = request.args.get("nome")
-    return render_template("index.html", nome=nome)
+    return render_template("index.html", username=nome)
 
 app.run(debug=True)
