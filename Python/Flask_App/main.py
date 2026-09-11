4fc2edccc71436d93459d542e4463d0971057e7c
Guilherme <guimars22@gmail.com>
2026-09-11 19:10:11 -0300

    #

--- Python/Flask_App/main.py
+++ Python/Flask_App/main.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-app.run()
+@app.route("/")
+def home():
+    nome = request.args.get("nome")
+    return render_template("index.html", nome=nome)
+
+app.run(debug=True)
