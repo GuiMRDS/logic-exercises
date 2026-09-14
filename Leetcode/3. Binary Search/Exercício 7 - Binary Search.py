35555a9516255e47ec0925f3a2035f0861619b0f
Guilherme <guimars22@gmail.com>
2026-09-14 13:27:47 -0300

    #

--- Leetcode/3. Binary Search/Exercício 7 - Binary Search.py
+++ Leetcode/3. Binary Search/Exercício 7 - Binary Search.py
@@ -5,3 +5,23 @@ target = 7
 # 3
 
 
+def BinarySearch(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        mid = (left+right) // 2
+
+        if nums[mid] == target:
+            return mid
+
+        if nums[mid] < target:
+            left = mid + 1
+
+        else:
+            right = mid - 1
+
+    return False
+
+
+print(BinarySearch(nums, target))
