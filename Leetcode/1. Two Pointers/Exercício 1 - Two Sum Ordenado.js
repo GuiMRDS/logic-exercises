2c29809b080a824e78d7d713f0b5415dd5a9e42c
Guilherme <guimars22@gmail.com>
2026-09-14 12:08:02 -0300

    #

---
+++ Leetcode/1. Two Pointers/Exercício 1 - Two Sum Ordenado.js
@@ -0,0 +1,11 @@
+nums = [0, 3, 2, 4];
+target = 6;
+
+// Resposta:
+// [2,3]
+
+function TwoPointer(nums, target) {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+}
