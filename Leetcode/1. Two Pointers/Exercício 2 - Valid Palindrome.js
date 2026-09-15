2199418518d04ce9efc08049da5181e3a39bc458
Guilherme <guimars22@gmail.com>
2026-09-15 11:00:22 -0300

    #

--- Leetcode/1. Two Pointers/Exercício 2 - Valid Palindrome.js
+++ Leetcode/1. Two Pointers/Exercício 2 - Valid Palindrome.js
@@ -1,4 +1,11 @@
 frase = "A man a plan a canal Panama";
 // True
 
+function isValidPalidrome(frase) {
+  left = 0;
+  right = frase.length - 1;
+
+  while (left > right) {}
+}
+
 console.log(isValidPalidrome(frase));
