66b2a2bb20dacc07ad80fb65d61c87edc52f904f
Guilherme <guimars22@gmail.com>
2026-09-15 10:56:59 -0300

    #

--- Leetcode/1. Two Pointers/Exercício 1 - Two Sum Ordenado.py
+++ Leetcode/1. Two Pointers/Exercício 1 - Two Sum Ordenado.py
@@ -5,6 +5,12 @@ target = 6
 # [2,3]
 
 def TwoPointerBruteForce(nums, target):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+
+    return False
     
 
 
