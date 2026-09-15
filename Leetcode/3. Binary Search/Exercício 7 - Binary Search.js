687eeba6d81911cee21482d6a91649d5546f2d74
Guilherme <guimars22@gmail.com>
2026-09-15 11:13:02 -0300

    #

--- Leetcode/3. Binary Search/Exercício 7 - Binary Search.js
+++ Leetcode/3. Binary Search/Exercício 7 - Binary Search.js
@@ -4,4 +4,23 @@ target = 7;
 // Resposta:
 // 3
 
+function BinarySeach(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = (left + right) / 2;
+
+    if (mid == target) {
+      return mid;
+    }
+
+    if (mid > target) {
+      left++;
+    } else {
+      right++;
+    }
+  }
+}
+
 console.log(BinarySeach(nums, target));
