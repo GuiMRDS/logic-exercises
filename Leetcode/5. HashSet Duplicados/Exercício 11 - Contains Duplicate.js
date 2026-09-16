4b85d6c54ac27351f799ddfb9a581d0c4ed7cc46
Guilherme <guimars22@gmail.com>
2026-09-16 15:04:24 -0300

    #

--- Leetcode/5. HashSet Duplicados/Exercício 11 - Contains Duplicate.js
+++ Leetcode/5. HashSet Duplicados/Exercício 11 - Contains Duplicate.js
@@ -1,4 +1,19 @@
 // [1,2,3,1]
 // true
 
+function HashSet(array) {
+  let visto = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    if (visto in array) {
+      return true;
+    }
+
+    visto.add(i);
+  }
+
+  return false;
+}
+
 console.log(HashSet([1, 2, 3, 1]));
+console.log(HashSet([1, 2, 3, 4]));
