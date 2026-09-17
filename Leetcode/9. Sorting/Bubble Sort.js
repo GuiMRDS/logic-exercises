cf1fb108145008e357a4995ea5cb5b3805a53da0
Guilherme <guimars22@gmail.com>
2026-09-17 13:56:26 -0300

    #

--- Leetcode/9. Sorting/Bubble Sort.js
+++ Leetcode/9. Sorting/Bubble Sort.js
@@ -1,13 +1,10 @@
-function bubleSort(nums) {
-  for (let i; i < nums.length; i++) {
-    for (let j; j < nums.length - i - 1; j++) {
-      if (nums[i] < nums[j + 1]) {
-        (nums[j], (nums[j + 1] = nums[j + 1]), nums[j]);
-      }
+function bubbleSort(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] > nums[j])
+        return (nums[j], (nums[j + 1] = nums[j + 1]), nums[j]);
     }
   }
-
-  return nums;
 }
 
-console.log(bubleSort([5, 4, 3, 2, 1]));
+console.log(bubbleSort([9, 8, 7, 6, 5, 4, 3, 2, 1]));
