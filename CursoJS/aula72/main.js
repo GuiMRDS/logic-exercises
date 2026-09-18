41f252964d1e570643daae20c899d192cfed7e35
Guilherme <guimars22@gmail.com>
2026-09-18 16:43:41 -0300

    #

--- CursoJS/aula72/main.js
+++ CursoJS/aula72/main.js
@@ -3,13 +3,25 @@ function Produto(nome, preco, estoque) {
   this.nome = nome;
   this.preco = preco;
 
+  let estoquePrivado = estoque;
+
   Object.defineProperty(this, "estoque", {
     enumerable: true, // mostrar chave
-    value: estoque, // valor
-    writable: true, // poder alterar
     configurable: true, // configurável
+    get: function () {
+      return estoque;
+    },
+    set: function (valor) {
+      if (typeof valor !== "number") {
+        throw new TypeError("Messagem");
+      }
+
+      estoquePrivado = valor;
+    },
   });
 }
 
 const p1 = new Produto("Camiseta", 20, 3);
-console.log(p1);
+// console.log(p1);
+p1.estoque = 500;
+console.log(p1.estoque);
